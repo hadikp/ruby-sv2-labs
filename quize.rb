5382737fbf7949e1b6fc1324579449a55c2cc48f
hadikp <hp20001025@gmail.com>
2022-01-02 22:44:59 +0100

    Build a quiz start

---
+++ quize.rb
@@ -0,0 +1,10 @@
+class Question
+  attr_accessor :prompt, :answer
+  def initialize(prompt, answer)
+    @prompt = prompt
+    @answer = answer
+  end
+end
+  p1 = "Whar color are apples?\n(a)red\n(b)purple\n(c)orange"
+  p2 = "Whar color are bananas?\n(a)pink\n(b)red\n(c)yellow"
+  p3 = "Whar color are pears?\n(a)yellow\n(b)green\n(c)orange"
