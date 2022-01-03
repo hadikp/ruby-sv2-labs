2328599b7ee43b088acccc700f3d90d45000eee3
hadikp <hp20001025@gmail.com>
2022-01-03 17:28:54 +0100

    Match operation

---
+++ match-operation.rb
@@ -0,0 +1,17 @@
+#Numbers
+ puts 2 * 3
+ puts 2**3
+ puts 10 % 3
+ puts (1 + 2) * 3
+ puts 10 / 3
+
+ num = 10
+ num += 100
+ puts num
+
+ num = -36.8
+ puts num.abs()
+ puts num.round()
+
+ puts Math.sqrt(144)
+ puts Math.log(1)
