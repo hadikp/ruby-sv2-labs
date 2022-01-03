dbb01dac9ef4f6ac601e6961c8f85eb90bba41ec
hadikp <hp20001025@gmail.com>
2022-01-03 13:49:55 +0100

    Quize

--- quize.rb
+++ quize.rb
@@ -8,3 +8,24 @@ end
   p1 = "Whar color are apples?\n(a)red\n(b)purple\n(c)orange"
   p2 = "Whar color are bananas?\n(a)pink\n(b)red\n(c)yellow"
   p3 = "Whar color are pears?\n(a)yellow\n(b)green\n(c)orange"
+
+questions = [
+  Question.new(p1, "a"),
+  Question.new(p2, "c"),
+  Question.new(p3, "b")
+]
+
+def run_test(questions)
+  answer = ""
+  score = 0
+  for question in questions
+    puts question.prompt
+    answer = gets.chomp()
+    if answer == question.answer
+      score += 1
+    end
+  end
+  puts ("You got " + score.to_s + "/" + questions.length().to_s)
+end
+
+run_test(questions)
