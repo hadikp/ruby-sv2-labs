52aedb050dfdcf3b27924f55b127d6ea27b23292
hadikp <hp20001025@gmail.com>
2022-01-04 17:51:40 +0100

    Calculate lifting 11.week day02

--- hiking.rb
+++ hiking.rb
@@ -1,12 +1,13 @@
-nums = Array[10, 20, 15, 18]
+nums = Array[10, 20, 15, 18] #Hegycsúcsok m-ben, feladat kiszámolni az emelkedést, ereszkedést ne számolja bele
 def getHiking(nums)
-  e = nums.each
-  first = e.next
-  second = e.next
-  if second > first
-    puts second - first
+  sum = 0;
+  for i in 0..nums.size-2 do
+      diff = (nums[i+1] - nums[i])
+      if (nums[i] < nums[i+1])
+        sum += diff
+      end
   end
-  puts e.next
+  puts sum
 end
 
 getHiking(nums)
